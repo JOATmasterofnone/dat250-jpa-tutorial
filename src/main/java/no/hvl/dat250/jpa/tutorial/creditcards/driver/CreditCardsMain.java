d129bfb135de597009446aca8e294f851afeb707
iRW <raive4603@uib.no>
2023-09-16 23:43:09 +0200

    report

--- src/main/java/no/hvl/dat250/jpa/tutorial/creditcards/driver/CreditCardsMain.java
+++ src/main/java/no/hvl/dat250/jpa/tutorial/creditcards/driver/CreditCardsMain.java
@@ -5,6 +5,11 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.util.*;
 
 public class CreditCardsMain {
@@ -18,10 +23,70 @@ public class CreditCardsMain {
       List<Object> createdObjects = createObjects(em);
       em.getTransaction().commit();
       //prettyPrintObjects(createdObjects);
+      inspectTheDatabase();
+
     } catch (Exception e) {
       e.printStackTrace();
     }
+
   }
+
+  private static void inspectTheDatabase(){
+
+      try {
+        // Load the H2 driver class
+        Class.forName("org.h2.Driver");
+
+        // Connect to the H2 database
+        Connection connection = DriverManager.getConnection("jdbc:h2:file:./DB;DB_CLOSE_DELAY=-1");
+
+        // Create a statement
+        Statement statement = connection.createStatement();
+
+        // Execute query to show tables
+        ResultSet resultSet = statement.executeQuery("SHOW TABLES");
+
+        // Print the names of the tables
+        while (resultSet.next()) {
+          String tableName = resultSet.getString("TABLE_NAME");
+          System.out.println("Table: " + tableName);
+
+
+          Statement contentStatement = connection.createStatement();
+          ResultSet contentResultSet = contentStatement.executeQuery("SELECT * FROM " + tableName);
+
+          ResultSetMetaData metaData = contentResultSet.getMetaData();
+          int columnCount = metaData.getColumnCount();
+
+          // Print column names
+          for (int i = 1; i <= columnCount; i++) {
+            System.out.print(metaData.getColumnName(i) + "\t");
+          }
+          System.out.println();
+
+          // Print rows
+          while (contentResultSet.next()) {
+            for (int i = 1; i <= columnCount; i++) {
+              System.out.print(contentResultSet.getString(i) + "\t");
+            }
+            System.out.println();
+          }
+
+          System.out.println("-----------------------------");
+          contentResultSet.close();
+          contentStatement.close();
+
+        }
+
+        // Close all resources
+        resultSet.close();
+        statement.close();
+        connection.close();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+  }
+
 
   private static void prettyPrintObjects(List<Object> createdObjects){
     for (Object obj : createdObjects) {
